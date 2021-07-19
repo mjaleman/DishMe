6cf503eb995cfc0aac6819b22c6f1d9abaca770f
Marlon Aleman <mjaleman1997@gmail.com>
2021-07-18 22:59:11 -0700

    Remove status bar on splash screen

--- app/src/main/java/com/projectmalthorn/dishme/SpalshActivity.kt
+++ app/src/main/java/com/projectmalthorn/dishme/SpalshActivity.kt
@@ -1,6 +1,9 @@
 package com.projectmalthorn.dishme
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.projectmalthorn.dishme.databinding.ActivitySpalshBinding
 
@@ -8,6 +11,16 @@ class SpalshActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spalsh)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         val splashBinding: ActivitySpalshBinding = ActivitySpalshBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
