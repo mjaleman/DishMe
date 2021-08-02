e6a2cc2ffd2880cf966b3500703fd4ad3ddf1a81
Marlon Aleman <mjaleman1997@gmail.com>
2021-08-01 19:52:45 -0700

    Enable view binding on AddUpdateDishActivity.kt
    This allows for easy access to elements in the xml file and added the ability to go to the previous screen when pressing the back button.

--- app/src/main/java/com/projectmalthorn/dishme/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/projectmalthorn/dishme/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,25 @@ package com.projectmalthorn.dishme.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.projectmalthorn.dishme.R
+import com.projectmalthorn.dishme.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
     }
+
+    private fun setupActionBar(){
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
 }
