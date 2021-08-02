ac192c1c69b7199781db88026b490f409fe36b2f
Marlon Aleman <mjaleman1997@gmail.com>
2021-08-01 20:13:36 -0700

    Test on click listener for add dish image button

--- app/src/main/java/com/projectmalthorn/dishme/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/projectmalthorn/dishme/view/activities/AddUpdateDishActivity.kt
@@ -1,18 +1,24 @@
 package com.projectmalthorn.dishme.view.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.projectmalthorn.dishme.R
 import com.projectmalthorn.dishme.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
-
         setContentView(mBinding.root)
+
+        setupActionBar()
+
+        mBinding.ivAddDishImage.setOnClickListener(this)
     }
 
     private fun setupActionBar(){
@@ -23,4 +29,15 @@ class AddUpdateDishActivity : AppCompatActivity() {
         }
     }
 
+    override fun onClick(v: View?) {
+        if(v != null){
+            when(v.id){
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "You have clicked the camera image",
+                        Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
+    }
 }
